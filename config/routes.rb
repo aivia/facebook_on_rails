9f0b0bd1d25466ff0d7ead098b78b1b8bce5fdb3
Allen Hu <aclhu@outlook.com>
2014-11-09 08:59:44 +1100

    Added a home page. added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 FacebookOnRails::Application.routes.draw do
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
+  #get "pages/home"
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
@@ -48,8 +50,8 @@ FacebookOnRails::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
+  root :to => 'pages#home'
+  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
