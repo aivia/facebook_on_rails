7f004982ccd57955411bc081f089e2c3128bad73
Allen Hu <aclhu@outlook.com>
2014-11-13 22:00:09 +1100

    Edited application controller, devise edit view, configured omniauth routes and added users omniauth controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  
+  before_fileter :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :exception
+
+  def configure_permitted_paramters
+  	devise_parameter_sanitizer.for(:sign_up) do |u|
+  		u.permit(
+  			:email,
+  			:name,
+  			:password,
+  			:password_confirmation
+  			)
+	  end
+
+  devise_parameter_sanitizer.for(:account_udpate) do |u|
+  	u.permit(
+  		:current_password,
+  		:email,
+  		:name,
+  		:password,
+  		:password_confirmation
+  		)
+  	end
+  end
 end
